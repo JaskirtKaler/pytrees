2d19250d5465a7e00c98e78f48130ce17951b19f
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2025-01-25 16:29:13 -0800

    traversal's

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class TreeNode:
         self.left = left
         self.right = right
     def add(self, val):
-        if val < root.val:
+        if val < self.val:
             if self.left is None:
                 self.left = TreeNode(val)
             else:
@@ -14,16 +14,66 @@ class TreeNode:
                 self.right = TreeNode(val)
             else:
                 self.right.add(val)
+    
+    # print tree inorder traversal
     def printTree(self):
         if self.left:
             self.left.printTree()
         print(self.val)
         if self.right:
             self.right.printTree()
+
+
+    def BST(self, value):
+        if self.val == value: # found value
+            return True
+        elif value < self.val: 
+            if self.left is None: # end search or keep moving
+                return False
+            else:
+                return self.left.BST(value)
+        else:  # value > self.val 
+            if self.right is None: # end search or keep moving
+                return False
+            else:
+                return self.right.BST(value)
+
+
+
+# Pre Order First center then left then right return lst
+def PreOrder(root):
+
+    def helper(node):
+        if node: 
+            res.append(node.val)
+            helper(node.left)
+            helper(node.right)
+    res = []
+    helper(root)
+    return res
+
+def PostOrder(root):
+    res = []
+    def helper(node):
+        if not node:
+            return
+        helper(node.left)
+        helper(node.right)
+        res.append(node.val)
+    helper(root)
+    return res
+
+
 if  __name__ == "__main__":
     root = TreeNode(3)
     root.right = TreeNode(5)
     root.left = TreeNode(1)
     root.add(-10)
-    print("Sample")
+    root.add(20)
+    root.add(-124)
+    root.add(56)
+    root.add(-5)
     root.printTree()
+    print(root.BST(10))
+    print(PreOrder(root))
+    print(PostOrder(root))
